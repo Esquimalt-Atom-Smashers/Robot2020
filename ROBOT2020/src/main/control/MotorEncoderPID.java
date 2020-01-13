2060681a46244c130067d502c608489948ef0d19
Cameron <47492133+Cameron-Mears@users.noreply.github.com>
2020-01-12 22:18:16 -0800

    pid

---
+++ ROBOT2020/src/main/control/MotorEncoderPID.java
@@ -0,0 +1,6 @@
+package frc.control;
+
+public class MotorEncoderPID extends PID
+{
+    
+}
---
+++ ROBOT2020/src/main/control/PID.java
@@ -0,0 +1,34 @@
+package frc.control;
+
+public class PID
+{
+    protected double derivative;
+
+    protected double integral;
+    protected double integralLimit;
+    protected double integralFunctionalRange;
+
+    protected double target;
+    protected double position;
+
+    protected double kI;
+    protected double kP;
+    protected double kD;
+
+    
+
+    public PID(double kI, double kP, double kD)
+    {
+
+    }
+
+    public double update()
+    {
+
+    }
+
+    public double calculate()
+    {
+
+    }
+}
