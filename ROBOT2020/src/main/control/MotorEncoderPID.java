6f5d628e8952a5f309453a8e7dbabb67e87e7a54
Cameron <47492133+Cameron-Mears@users.noreply.github.com>
2020-01-16 09:30:52 -0800

    encoder pid

--- ROBOT2020/src/main/control/MotorEncoderPID.java
+++ ROBOT2020/src/main/control/MotorEncoderPID.java
@@ -1,6 +1,20 @@
 package frc.control;
 
+import edu.wpi.first.wpilibj.Encoder;
+
 public class MotorEncoderPID extends PID
 {
-    
+    private Encoder encoder;
+
+    public MotorEncoderPID(double kI, double kP, double kD, double integralLimit, double integralFunctionRange, Encoder encoder)
+    {
+        super(kI,kP,kD,integralFunctionRange,integralLimit);
+        this.encoder = encoder;
+    }
+
+    @Override
+    public double getPosition()
+    {
+        return (double) encoder.get();
+    }
 }
