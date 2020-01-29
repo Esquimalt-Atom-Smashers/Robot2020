eb54e6569ee175d31d93f199b2414da5a388810e
Cameron <47492133+Cameron-Mears@users.noreply.github.com>
2020-01-29 09:08:53 -0800

    math

--- ROBOT2020/src/main/java/frc/util/MathExtended.java
+++ ROBOT2020/src/main/java/frc/util/MathExtended.java
@@ -26,7 +26,10 @@ public interface MathExtended
     {
         double[] solutions = new double[2];
 
+        if (Math.pow(b,2) + 4*a*c < 0) return null; //check for negative discrimant
+
         solutions[0] = (-b+Math.sqrt(Math.pow(b, 2) + 4*a*c))/2*a;
+
         solutions[1] = (-b-Math.sqrt(Math.pow(b, 2) + 4*a*c))/2*a;
 
         return solutions;
