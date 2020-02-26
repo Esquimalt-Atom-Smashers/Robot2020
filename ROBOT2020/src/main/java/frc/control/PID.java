c40920b2429258b42fdf7c356dcc5fab3b92eae4
Cameron <47492133+Cameron-Mears@users.noreply.github.com>
2020-02-25 22:19:26 -0800

    added rate limiting and acceleration limiting to PIDs should make things more smooth

--- ROBOT2020/src/main/java/frc/control/PID.java
+++ ROBOT2020/src/main/java/frc/control/PID.java
@@ -7,6 +7,18 @@ import edu.wpi.first.wpilibj.DriverStation;
 public class PID
 {
     protected double derivative;
+    protected double lastDerivative;
+    protected double derivativeIntegral;
+    protected double maxRate;
+    protected double kRateP;
+    protected double kRateI;
+    protected double kRateD;
+
+    protected boolean doRateLimit = false;
+
+    protected double maxPowerChangePerTick;
+    protected double lastPower;
+
 
     protected double integral;
     protected double integralLimit;
@@ -15,6 +27,8 @@ public class PID
     protected double target;
     protected double position;
     protected long lastCallTime;
+
+     
 
     protected double kI;
     protected double kP;
@@ -34,10 +48,47 @@ public class PID
 
     }
 
+    public void setMaxPowerChangePerTick(double max)
+    {
+        this.maxPowerChangePerTick = max;
+    }
+
     public void setTarget(double target)
     {
         this.target = target;
     }
+
+    public void enableRateLimiting(boolean useRateLimiting)
+    {
+        this.doRateLimit = useRateLimiting;
+    }
+
+    public void initRateLimiting(double kRateP, double kRateI, double kRateD, double maxRate, boolean enableNow)
+    {
+        this.kRateP = kRateP;
+        this.kRateI = kRateI;
+        this.kRateD = kRateD;
+        this.maxRate = maxRate;
+        this.doRateLimit = enableNow;
+    }
+
+    private double calculateRateAdjustmentPower(double derivative, double deltaT)
+    {
+        double rateError = derivative * maxRate;
+
+        double secondDerivative = (derivative - lastDerivative)/deltaT;
+        lastDerivative = derivative;
+        double powerReduction = secondDerivative * kRateD;
+        powerReduction += rateError * kRateP;
+
+        derivativeIntegral += rateError * deltaT;
+
+        powerReduction += derivativeIntegral * kRateI;
+
+        return powerReduction;
+
+   }
+
 
     public double getPosition()
     {
@@ -55,8 +106,8 @@ public class PID
         position = getPosition();
         long time = System.currentTimeMillis();
         double delta = (lastCallTime - time)/1d;
+        derivative = positionChange/delta;
         lastCallTime = time;
-        double derivative = positionChange/delta;
         
         double error =  target - position;
 
@@ -72,6 +123,14 @@ public class PID
 
         power += error * kP;
         power += derivative * kI;
+
+        if (doRateLimit) power -= calculateRateAdjustmentPower(derivative, delta);
+
+        double deltaPower = power - lastPower;
+        if (Math.abs(deltaPower) > maxPowerChangePerTick)
+        {
+            power = lastPower + (maxPowerChangePerTick * Math.signum(deltaPower));
+        }
 
         return power;
     }
