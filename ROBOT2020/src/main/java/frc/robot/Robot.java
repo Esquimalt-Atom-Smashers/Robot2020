b91ef33b700473d13fa3fa269fafd3855791aacb
EsqAtomSmashersProgramers <32209970+Sarkhan-the-man@users.noreply.github.com>
2020-03-01 13:05:14 -0800

    Marek's work in progress Motor names, going to be renammed later

--- ROBOT2020/src/main/java/frc/robot/Robot.java
+++ ROBOT2020/src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.subsystem.CLP;
+import edu.wpi.first.hal.PowerJNI;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.I2C;
@@ -40,7 +41,11 @@ public class Robot extends TimedRobot {
    * for any initialization code.
    */
 
-  
+  private WPI_VictorSPX vic1;
+  private WPI_VictorSPX vic2;
+  private WPI_VictorSPX vic3;
+  private WPI_VictorSPX vic4;
+  private WPI_VictorSPX vic5;
   private RobotServer server;
   private DifferentialDrive drive;
   private XboxController xcontroller;
@@ -63,23 +68,28 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     
-   
+    vic1 = new WPI_VictorSPX(1);
+    vic2 = new WPI_VictorSPX(2);
+    vic3 = new WPI_VictorSPX(3);
+    vic4 = new WPI_VictorSPX(4);
+    vic5 = new WPI_VictorSPX(5);
     //compressor = new Compressor(0);
     //clp = new CLP(testadoo, testaboo);
     //test = new Spark(0);
     xcontroller = new XboxController(3);
-    //colorsensor = new ColorSensorV3(i2cPort);
+    colorsensor = new ColorSensorV3(i2cPort);
     talon = new WPI_TalonSRX(0);
-    try {
-      drive = new DifferentialDrive(new Spark(0), new Spark(1)); // the base and wheels
-      stick = new Joystick(3); // controls spike
-      drive.setInverted(true);
-    } catch (Exception e) {
-      System.out.println("Error communicating with drive, joystick not available");
+   // try {
+      //drive = new DifferentialDrive(new Spark(0), new Spark(1)); // the base and wheels
+      //stick = new Joystick(3); // controls spike
+      //drive.setInverted(true);
+  //  } catch (Exception e) {
+    //  System.out.println("Error communicating with drive, joystick not available");
     }
+
    
 
-  }
+//}
 
   @Override
   public void autonomousInit() {
@@ -105,22 +115,26 @@ public class Robot extends TimedRobot {
 
     //test.set(Math.max(0,ledpower));
 
-    double tal = xcontroller.getRawAxis(1);
+    //double tal = xcontroller.getRawAxis(1);
     //talon.set(tal);
 
-    //Color detectedColor = colorsensor.getColor();
-  
+    double v1 = xcontroller.getRawAxis(1);
+    vic1.set(v1);
+    //double v2 = xcontroller.getRawAxis(1);
+    //vic2.set(v2);
+    //double v3 = xcontroller.getRawAxis(3);
+    //vic3.set(v3);
+    //double v4 = xcontroller.getRawAxis(3);
+    //vic4.set(v4);
+    //double v5 = xcontroller.getRawAxis(3);
+    //vic5.set(v5);
 
-
-    //double IR = colorsensor.getIR(); 
-
-    //int proximity = colorsensor.getProximity();
     
-    //System.out.println(detectedColor);
-    //SmartDashboard.putNumber("Proximity", proximity);
-    try {
-        drive.arcadeDrive(xcontroller.getRawAxis(1), xcontroller.getRawAxis(2)); // move around
-    } catch (Exception e) {}
+
+  
+ //   try {
+ //       drive.arcadeDrive(xcontroller.getRawAxis(1), xcontroller.getRawAxis(2)); // move around
+ //   } catch (Exception e) {}
     
   }
 
