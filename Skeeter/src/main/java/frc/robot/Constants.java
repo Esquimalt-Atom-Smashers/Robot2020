6a169ce721549e7ce75a702333c5ac45cb022c8f
Isaac4321 <74685042+Isaac4321@users.noreply.github.com>
2021-01-20 19:35:35 -0800

    Update Constants.java

--- Skeeter/src/main/java/frc/robot/Constants.java
+++ Skeeter/src/main/java/frc/robot/Constants.java
@@ -12,28 +12,35 @@ public class Constants
  | |___| (_) | | | \__ \ || (_| | | | | |_\__ \
   \_____\___/|_| |_|___/\__\__,_|_| |_|\__|___/
                                               
+     Note: I will be commenting where you can find the variable be used once i find where they are located - Isaac Nelson
     */
-    
-    //Keep in mind constants are just variables that cannot be changed.
     
     //Declares a public constant int named "SPARK_LEFT_DRIVE_PWM" and sets the value to 0
     public static final int SPARK_LEFT_DRIVE_PWM = 0;
     //Declares a public constant int named "SPARK_RIGHT_DRIVE_PWM" and sets the value to 1
     public static final int SPARK_RIGHT_DRIVE_PWM = 1;
 
-    
+    //Declares a public constant int named "CLP_DRIVE_VICTORSPX_CAN_ID" and sets the value to 6
     public static final int CLP_DRIVE_VICTORSPX_CAN_ID = 6;
+    //Declares a public constant int named "CLP_DRIVE_LIMITSWITCH_LOWER" and sets the value to 1
     public static final int CLP_DRIVE_LIMITSWITCH_LOWER = 1;
+    //Declares a public constant int named "CLP_DRIVE_LIMITSWITCH_UPPER" and sets the value to 2
     public static final int CLP_DRIVE_LIMITSWITCH_UPPER = 2;
 
+    //Declares a public constant double named "CLP_DRIVE_GEARING" and sets the value to 180.0d
     public static final double CLP_DRIVE_GEARING = 180d;
-
+    //Declares a public constant double named "CIM_ENCODER_PPR" and sets the value to 20.0d
     public static final double CIM_ENCODER_PPR = 20d;
 
+    //Declares a public constant int named "CLP_SHOOTER1_VICTORSPX_CAN_ID" and sets the value to 4
     public static final int CLP_SHOOTER1_VICTORSPX_CAN_ID = 4;
+    //Declares a public constant int named "CLP_SHOOTER2_VICTORSPX_CAN_ID" and sets the value to 5
     public static final int CLP_SHOOTER2_VICTORSPX_CAN_ID = 5;
+    //Declares a public constant int named "CLP_TRANSPORT_COLLECTOR_VICTORSPX_CAN_ID" and sets the value to 1
     public static final int CLP_TRANSPORT_COLLECTOR_VICTORSPX_CAN_ID = 1;
+    //Declares a public constant int named "CLP_TRANSPORT_CENTER_VICTORSPX_CAN_ID" and sets the value to 2
     public static final int CLP_TRANSPORT_CENTER_VICTORSPX_CAN_ID = 2;
+    //Declares a public constant int named "CLP_TRANSPORT_LAUNCH_VICTORSPX_CAN_ID" and sets the value to 3
     public static final int CLP_TRANSPORT_LAUNCH_VICTORSPX_CAN_ID = 3;
     
 
