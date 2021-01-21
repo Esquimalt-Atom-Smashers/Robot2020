ff8989e0db0ad71bf01120e25198eeee09afd45e
Isaac4321 <74685042+Isaac4321@users.noreply.github.com>
2021-01-20 19:26:38 -0800

    Update Constants.java

--- Skeeter/src/main/java/frc/robot/Constants.java
+++ Skeeter/src/main/java/frc/robot/Constants.java
@@ -1,11 +1,27 @@
 //This line tells the compiler that Constants.java is part of the frc.robot package.
 package frc.robot;
 
+//Creates a public class called Constants. Note that the name of the class is the same as the file name.
 public class Constants
 {
+    /*
+   _____                _              _       
+  / ____|              | |            | |      
+ | |     ___  _ __  ___| |_ __ _ _ __ | |_ ___ 
+ | |    / _ \| '_ \/ __| __/ _` | '_ \| __/ __|
+ | |___| (_) | | | \__ \ || (_| | | | | |_\__ \
+  \_____\___/|_| |_|___/\__\__,_|_| |_|\__|___/
+                                              
+    */
+    
+    //Keep in mind constants are just variables that cannot be changed.
+    
+    //Declares a public constant int named "SPARK_LEFT_DRIVE_PWM" and sets the value to 0
     public static final int SPARK_LEFT_DRIVE_PWM = 0;
+    //Declares a public constant int named "SPARK_RIGHT_DRIVE_PWM" and sets the value to 1
     public static final int SPARK_RIGHT_DRIVE_PWM = 1;
 
+    
     public static final int CLP_DRIVE_VICTORSPX_CAN_ID = 6;
     public static final int CLP_DRIVE_LIMITSWITCH_LOWER = 1;
     public static final int CLP_DRIVE_LIMITSWITCH_UPPER = 2;
