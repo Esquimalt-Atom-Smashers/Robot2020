612ccca0bc0f84ca88a5f4ece402f61447b4c679
Isaac4321 <74685042+Isaac4321@users.noreply.github.com>
2021-01-20 19:12:00 -0800

    Update ArduinoSensorBoard.java

--- Skeeter/src/main/java/frc/can/ArduinoSensorBoard.java
+++ Skeeter/src/main/java/frc/can/ArduinoSensorBoard.java
@@ -1,15 +1,30 @@
+//This line tells the compiler that ArduinSensorBoard.java is part of the frc.can package.
 package frc.can;
-//imports the class CAN so ArduinoSensorBoard has access to all CAN methods
+
+//Imports the class CAN so ArduinoSensorBoard has access to all CAN methods.
 import edu.wpi.first.wpilibj.CAN;
 
+//Creates a public class named "ArduinoSensorBoard", keep in mind that the name of this class is the name of the java file .
 public class ArduinoSensorBoard
 {
+    /*Creates a private final int named "CAN_TEAM_USE" and is assigned the int 8.
+    This means the value of this integer cant be changed and only ArdunioSensorBoard has access to this variable.*/
     private static final int CAN_TEAM_USE = 8;
+    
+    /*Creates a private final int named "CAN_TYPE_MISCELLANEOUS" and is assigned the int 10.
+    This means the value of this integer cant be changed and only ArdunioSensorBoard has access to this variable.*/
     private static final int CAN_TYPE_MISCELLANEOUS = 10;
+    
+    //Creates a private CAN variable(CAN just allows to interface with CAN devices) called "board". 
     private CAN board;
 
+    //Creates a public class called "ArduinoSensorBoard" which takes in 1 parameter. The parameter is an integer variable called "deviceID".
     public ArduinoSensorBoard(int deviceID)
     {
+        /*  "board" is made into an object and creates a CAN communication interface with the specific (int deviceID, int deviceManufactuar, int DeviceType)
+        In this case (deviceID) is equal to whatever integer is placed in ArduinoSensorBoard's parameter.
+        And the (deviceManufactuar) parameter is the "CAN_TEAM_USED" variable we made up above so it is equal to 8
+        Lastly the (DeviceType) parameter is the "CAN_TYPE_MISCELLANEOUS" variable so its value is equal to 10  */
         board = new CAN(deviceID, CAN_TEAM_USE, CAN_TYPE_MISCELLANEOUS);
     }
 
