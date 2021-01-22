96d04c33a99c35c47a97c483a15f559d74ee7fd7
Isaac4321 <74685042+Isaac4321@users.noreply.github.com>
2021-01-21 22:13:49 -0800

    Update Collector.java

--- Skeeter/src/main/java/frc/subsystem/Collector.java
+++ Skeeter/src/main/java/frc/subsystem/Collector.java
@@ -1,17 +1,20 @@
 package frc.subsystem;
 
 public class Collector {
+    //This is Collector's constructor
     public Collector(){
         System.out.println("Collector constructor");
     }
-
+    /*creates a public method named 'collect' which returns a boolean 
+    This method will be called when we want the collection mode to be turned on*/
     public boolean collect() {
         System.out.println("Collection mode is on!");
         deployCollector();
         collectorRotationOn();
         return true;
     }
-
+    /*creates a public method named 'stopcollect' which returns a boolean 
+    This method will be called when we want the collection mode to be turned off*/
     public boolean stopCollect() {
         System.out.println("Collection mode is off!");
         withdrawCollector();
@@ -27,8 +30,20 @@ public class Collector {
     }
     public void collectorRotationOn(){
 
+        /*
+        * Put code in here to turn on the moters which control the rotation of the collecter
+        *
+        *
+        */
+        
     }
     public void collectorRotationOff(){
 
+        /*
+        * Put code in here to turn off the moters which control the rotation of the collecter
+        *
+        *
+        */
+        
     }
 }
