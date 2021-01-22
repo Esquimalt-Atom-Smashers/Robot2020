b6b9fa71c09a4b61bfa3f2b2c35a96537c617578
Isaac4321 <74685042+Isaac4321@users.noreply.github.com>
2021-01-21 22:39:55 -0800

    Update MotorEncoderPID.java

--- Skeeter/src/main/java/frc/control/MotorEncoderPID.java
+++ Skeeter/src/main/java/frc/control/MotorEncoderPID.java
@@ -2,6 +2,12 @@ package frc.control;
 
 import edu.wpi.first.wpilibj.Encoder;
 
+/*  
+*   To wrap this code up in a nutshell, essentially the Encoder class is used so we can read quadrature encoders.
+*   Quadrature encoders are devices that can count shaft rotation and sense direction.
+*   The output of the Quadrature encoders will be a integer that can cound up or down, negative being reverse.
+*
+*/
 public class MotorEncoderPID extends PID
 {
     private Encoder encoder;
